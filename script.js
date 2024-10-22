a848b2eb3f8004fa566828214121acc5ee7d69dd
armaneey <imanshuaibu1@gmail.com>
2024-10-22 03:49:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById('menu-btn');
+const sideMenu = document.getElementById('side-menu');
+
+
+    menuBtn.addEventListener('click', () => {
+        sideMenu.classList.toggle('active');
+    });
+    
